55e2202514424d01fe40db0a0749a270a9ecb306
Éric Araujo <earaujo@caravan.coop>
2017-10-19 16:48:40 -0400

    address flake8 warnings

--- configstore/__init__.py
+++ configstore/__init__.py
@@ -1,3 +1,8 @@
 from configstore.backends import DockerSecretBackend, EnvVarBackend
 from configstore.exceptions import ConfigNotFoundException
 from configstore.store import Store
+
+__all__ = [
+    'Store', 'ConfigNotFoundException',
+    'EnvVarBackend', 'DockerSecretBackend',
+]
--- configstore/backends/__init__.py
+++ configstore/backends/__init__.py
@@ -1,2 +1,4 @@
 from configstore.backends.docker_secret import DockerSecretBackend
 from configstore.backends.env_var import EnvVarBackend
+
+__all__ = ['EnvVarBackend', 'DockerSecretBackend']
