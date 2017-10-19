eeab2a7e0c4c8f1ef84085a2ec07ae7a99c8b04d
Antoine Reversat <antoine@caravan.coop>
2017-10-19 16:58:33 -0400

    Simplify get_config

--- configstore/backends/env_var.py
+++ configstore/backends/env_var.py
@@ -4,7 +4,4 @@ import os
 class EnvVarBackend(object):
 
     def get_config(self, config):
-        try:
-            return os.environ[config]
-        except KeyError:
-            return None
+        return os.environ.get(config)
