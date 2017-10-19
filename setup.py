9bc0f55e1762ec471724e776609983544f55a102
Antoine Reversat <antoine@caravan.coop>
2017-10-19 14:48:37 -0400

    Update setup.py with real url + testing info

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup(name='configstore',
       version='0.1',
       description='Retrieve config from different backends',
-      url='',
+      url='https://github.com/caravancoop/configstore.git',
       author='Antoine Reversat',
       author_email='antoine@caravan.coop',
       license='MIT',
-      packages='configstore',
+      packages=['configstore'],
+      test_suite='nose.collector',
+      test_require=['nose'],
       zip_safe=False)
