6093b4a45a78c581ec8b6b850843ac9cd289ced7
Antoine Reversat <antoine@caravan.coop>
2017-10-19 15:20:08 -0400

    Add an example with a default

--- example.py
+++ example.py
@@ -5,4 +5,8 @@ configstore.enabled_backends = [
     configstore.EnvVarBackend()
 ]
 
+# Will throw an exception if APP_SECRET_KEY can't be found in any backend
 SECRET_KEY = configstore.get_config('APP_SECRET_KEY')
+# Will set the default value as the value if APP_SECRET_KEY can't be found
+# in any backend
+SECRET_KEY = configstore.get_config('APP_SECRET_KEY', 'defaultvalue')
