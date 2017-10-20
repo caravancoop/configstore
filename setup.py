6903a8c171cd19d03617bd89229ad9ed76dd24d8
Éric Araujo <earaujo@caravan.coop>
2017-10-20 10:30:25 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='configstore',
-    version='0.1',
+    version='0.2',
     description='Retrieve config from different backends',
     url='https://github.com/caravancoop/configstore',
     author='Antoine Reversat',
