f110c5609ee463de8cbd875a9e5413ae6d5f8c68
Éric Araujo <earaujo@caravan.coop>
2018-02-09 13:16:36 -0500

    add add_backend method

--- configstore/store.py
+++ configstore/store.py
@@ -8,10 +8,13 @@ _no_default = object()
 class Store(object):
 
     def __init__(self, backends):
-        self.backends = list(backends)
+        self._backends = tuple(backends)
+
+    def add_backend(self, backend):
+        self._backends += (backend,)
 
     def get_setting(self, key, default=_no_default):
-        for backend in self.backends:
+        for backend in self._backends:
             ret = backend.get_setting(key)
             if ret is None:
                 continue
