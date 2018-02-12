e394ddaddc87306b88f0db658b48082e99d20b8f
Éric Araujo <earaujo@caravan.coop>
2018-02-11 19:49:08 -0500

    update example

--- example.py
+++ example.py
@@ -1,13 +1,15 @@
 import configstore
 
-backends = [
+store = configstore.Store([
     configstore.DockerSecretBackend(),
-    configstore.EnvVarBackend()
-]
+    configstore.EnvVarBackend(),
+])
 
-store = configstore.Store(backends)
 # Will throw an exception if APP_SECRET_KEY can't be found in any backend
 SECRET_KEY = store.get_setting('APP_SECRET_KEY')
+
 # Will set the default value as the value if APP_SECRET_KEY can't be found
 # in any backend
 SECRET_KEY = store.get_setting('APP_SECRET_KEY', 'defaultvalue')
+
+del store
