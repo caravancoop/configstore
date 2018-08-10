86352b7027056d3e804cff9cc9d5c72202c16efb
Éric Araujo <earaujo@caravan.coop>
2018-08-10 15:29:36 -0400

    test with python3.7

--- example.py
+++ example.py
@@ -5,11 +5,10 @@ store = configstore.Store([
     configstore.EnvVarBackend(),
 ])
 
-# Will throw an exception if APP_SECRET_KEY can't be found in any backend
+# Will raise an exception if APP_SECRET_KEY isn't found in any backend
 SECRET_KEY = store.get_setting('APP_SECRET_KEY')
 
-# Will set the default value as the value if APP_SECRET_KEY can't be found
-# in any backend
+# Will use 'defaultvalue' if APP_SECRET_KEY isn't found
 SECRET_KEY = store.get_setting('APP_SECRET_KEY', 'defaultvalue')
 
 del store
