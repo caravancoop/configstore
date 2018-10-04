6995f54c02069bea2ab079f6e1c0cc3d3208bd20
JulienLabonte <julien@caravan.coop>
2018-10-04 13:29:51 -0400

    Adding optional path to init.

--- configstore/backends/awsssm.py
+++ configstore/backends/awsssm.py
@@ -6,12 +6,13 @@ except ImportError:
 
 class AwsSsmBackend(object):
 
-    def __init__(self):
+    def __init__(self, path=''):
         if boto3 is None:
             raise ImportError('install configstore[awsssm] to use '
                               'the awsssm backend')
+        self.path = path
 
     def get_setting(self, param):
         client = boto3.client('ssm')
-        response = client.get_parameter(Name=param)
+        response = client.get_parameter(Name=self.path+param)
         return response['Parameter']['Value']
