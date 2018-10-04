a037a9305bc18330b0be8cc67cbcc7d51fc6b9c3
JulienLabonte <julien@caravan.coop>
2018-10-04 14:00:10 -0400

    Adding decryption true.

--- configstore/backends/awsssm.py
+++ configstore/backends/awsssm.py
@@ -6,13 +6,13 @@ except ImportError:
 
 class AwsSsmBackend(object):
 
-    def __init__(self, path=''):
+    def __init__(self, name_prefix=''):
         if boto3 is None:
             raise ImportError('install configstore[awsssm] to use '
                               'the awsssm backend')
-        self.path = path
+        self.name_prefix = name_prefix
 
     def get_setting(self, param):
         client = boto3.client('ssm')
-        response = client.get_parameter(Name=self.path+param)
+        response = client.get_parameter(Name=self.name_prefix+param, WithDecryption=True)
         return response['Parameter']['Value']
