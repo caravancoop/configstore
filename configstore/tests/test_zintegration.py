97ae4aad38b6c762722eb8bdd9f2be818c7c2d2d
JulienLabonte <julien@caravan.coop>
2018-10-19 14:37:39 -0400

    Add test for export items in .env

--- configstore/tests/test_zintegration.py
+++ configstore/tests/test_zintegration.py
@@ -4,6 +4,7 @@ import configstore
 DOTENV_CONTENTS = u'''
 SECRET_KEY=1234dot
 ENVIRONMENT=PRODUCTION
+export DEBUG=True
 '''
 
 
@@ -17,8 +18,8 @@ def test_env_var_dotenv_success(monkeypatch, tmpdir):
         configstore.DotenvBackend(str(p)),
     ])
 
-    environment = store.get_setting('ENVIRONMENT')
     secret_key = store.get_setting('SECRET_KEY')
+    environment = store.get_setting('ENVIRONMENT')
 
     assert secret_key == '1234dot'
     assert environment == 'STAGING'
@@ -36,10 +37,25 @@ def test_dotenv_env_var_success(monkeypatch, tmpdir):
         configstore.EnvVarBackend(),
     ])
 
-    environment = store.get_setting('ENVIRONMENT')
     secret_key = store.get_setting('SECRET_KEY')
+    environment = store.get_setting('ENVIRONMENT')
 
     assert secret_key == '1234dot'
     assert environment == 'PRODUCTION'
+
+    del store
+
+
+def test_export_success(tmpdir):
+    p = tmpdir.join("config.env")
+    p.write(DOTENV_CONTENTS)
+
+    store = configstore.Store([
+        configstore.DotenvBackend(str(p)),
+    ])
+
+    debug = store.get_setting('DEBUG')
+
+    assert debug == 'True'
 
     del store
