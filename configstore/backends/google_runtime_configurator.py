5c2c0cdc99b9ee920e2dd9ea9e428662d569c58c
Sacha Manson-Smith <sachams@users.noreply.github.com>
2023-01-03 13:00:40 +0000

    Add caching of keys to google backends (#2)

--- configstore/backends/google_runtime_configurator.py
+++ configstore/backends/google_runtime_configurator.py
@@ -8,15 +8,41 @@ class GoogleRuntimeConfiguratorBackend:
     """Backend for Google Runtime Configurator.
     """
 
-    def __init__(self, project_id, config_name):
+    def __init__(self, project_id: str, config_name: str, cache_keys: bool):
         client = runtimeconfig.Client(project=project_id)
         self.config = client.config(config_name)
 
-    def get_setting(self, key):
-        """Returns the named item from runtime configurator"""
+        # If cache_keys is true, we load a list of all keys at init time. This
+        # will make things a lot quicker when trying to load a key that doesn't exist
+        if cache_keys:
+            self.cached_keys = self._list()
+        else:
+            self.cached_keys = None
+
+    def _list(self) -> set[str]:
+        """Lists all the keys of items stored in an environment"""
+        variables = self.config.list_variables()
+        return {variable.name for variable in list(variables)}
+
+    def _get(self, key) -> str:
+        """Low level method to get setting from runtime configurator"""
         variable = self.config.get_variable(key)
 
         if variable is None:
             return None
         else:
             return variable.text
+
+    def get_setting(self, key) -> str:
+        """Returns the named item from runtime configurator,
+        optionally checking a cached list of keys that exist before doing
+        so"""
+
+        # If we have cached a list of keys, check that list before
+        # trying to load a variable. This will speed things up a lot
+        # in the case where the key doesn't exist
+        if self.cached_keys is not None:
+            if key not in self.cached_keys:
+                return None
+
+        return self._get(key)
--- configstore/backends/google_secret_manager.py
+++ configstore/backends/google_secret_manager.py
@@ -10,12 +10,24 @@ class GoogleSecretManagerBackend:
     """Backend for Google Secrets Manager (GSM).
     """
 
-    def __init__(self, project_id):
+    def __init__(self, project_id: str, cache_keys: str):
         self.project_id = project_id
         self.client = secretmanager.SecretManagerServiceClient()
 
-    def get_setting(self, key):
-        """Returns the named secret from GSM"""
+        # If cache_keys is true, we load a list of all keys at init time. This
+        # will make things a lot quicker when trying to load a key that doesn't exist
+        if cache_keys:
+            self.cached_keys = self._list()
+        else:
+            self.cached_keys = None
+
+    def _list(self) -> set[str]:
+        """Lists all the names of secrets stored in an environment"""
+        parent = f"projects/{self.project_id}"
+        results = self.client.list_secrets(parent=parent)
+        return {response.name.split("/")[-1] for response in results}
+
+    def _get(self, key: str) -> str:
         full_name = f"projects/{self.project_id}/secrets/{key}/versions/latest"
 
         # Access the secret version
@@ -26,3 +38,16 @@ class GoogleSecretManagerBackend:
 
         # Return the decoded payload
         return response.payload.data.decode("UTF-8")
+
+    def get_setting(self, key: str) -> str:
+        """Returns the named item from GSM,
+        optionally checking a cached list of keys that exist before doing
+        so"""
+        # If we have cached a list of keys, check that list before
+        # trying to load a variable. This will speed things up a lot
+        # in the case where the key doesn't exist
+        if self.cached_keys is not None:
+            if key not in self.cached_keys:
+                return None
+
+        return self._get(key)
